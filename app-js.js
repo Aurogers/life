9ffe6a292ec39053fdadc6e51a47c8d85ec1c314
Aurogers <70723781+Aurogers@users.noreply.github.com>
2025-01-02 18:49:03 -0700

    Add files via upload

---
+++ app-js.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LifeDashboard from './components/LifeDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <LifeDashboard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
